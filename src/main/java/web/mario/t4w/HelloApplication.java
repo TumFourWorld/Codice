97fac75d11bac139c9de9bf71e13d82ffaa1bd15
mario vaccaro <mario.vaccaro@studenti.unitn.it>
2023-05-11 16:51:09 +0200

    mario

---
+++ src/main/java/web/mario/t4w/HelloApplication.java
@@ -0,0 +1,9 @@
+package web.mario.t4w;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
